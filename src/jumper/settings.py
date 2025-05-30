44ae4717100d9b49e179a5f01e4028939ff99817
Antoine Libert <contact@antoine-libert.com>
2025-05-30 11:51:44 +0200

    fix: AUTH_COOKIE configuration

--- src/jumper/settings.py
+++ src/jumper/settings.py
@@ -156,10 +156,10 @@ SIMPLE_JWT = {
     "AUTH_COOKIE_DOMAIN": None,
     "AUTH_COOKIE": "access_token",
     "AUTH_COOKIE_REFRESH": "refresh_token",
-    "AUTH_COOKIE_SECURE": False,
+    "AUTH_COOKIE_SECURE": True,
     "AUTH_COOKIE_HTTP_ONLY": False,
     "AUTH_COOKIE_PATH": "/",
-    "AUTH_COOKIE_SAMESITE": None,
+    "AUTH_COOKIE_SAMESITE": "None",
 }
 
 # Password validation
