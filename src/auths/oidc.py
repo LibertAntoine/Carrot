c579e117b121a403fd8ecd1261a5f01f524cbe53
Antoine Libert <contact@antoine-libert.com>
2025-06-02 14:20:34 +0200

    fix: oidc redirect_url

--- src/auths/oidc.py
+++ src/auths/oidc.py
@@ -12,8 +12,11 @@ from .jwt import set_jwt_tokens, get_tokens_for_user
 
 class OIDCAuthCallback(OIDCAuthenticationCallbackView):
     def login_success(self):
-        response = HttpResponseRedirect(self.success_url)
         tokens = get_tokens_for_user(self.user)
+        # TODO: optionally redirect to jumper, to allow oidc auth from another app
+        response = HttpResponseRedirect(
+            f"jumper://oidc-auth-callback?status=success&access_token={tokens['access']}&refresh_token={tokens['refresh']}"
+        )
         set_jwt_tokens(tokens, response, self.request, with_refresh=False)
         response.delete_cookie("sessionid", path="/")
         return response
